e184e3898362630f89c1411709317d0c0732f16f
johnLeavell <johnleavell@gmail.com>
2023-09-05 13:30:46 -0500

    added directors home and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get '/directors', to: 'directors#home'
+  get 'directors/:id', to: 'directors#show'
 end
